9dd95723e74e8aa87f68d4f4223edffd0481fb63
michalhejd <71251670+michalhejd@users.noreply.github.com>
2022-05-07 16:17:49 +0200

    addtocart

--- src/views/itemDetail.vue
+++ src/views/itemDetail.vue
@@ -7,15 +7,35 @@
       <p>{{ product.name }}</p>
       <p>quantity: {{ product.quantity }}</p>
       <p>{{ product.price }}&euro;</p>
+      <button @click="addToCart()">Add to cart</button>
     </div>
   </div>
 </template>
 <script>
+import axios from "axios";
 import navigation from "@/components/navigation.vue";
 export default {
   name: "itemDetail",
   components: {
     navigation,
+  },
+  methods: {
+    async addToCart() {
+      await axios
+        .post(
+          "http://localhost:3000/user/cart/add", {
+            headers: {
+              Authorization: `Bearer ${this.$store.state.token}`,
+            },
+          }
+        )
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
   },
   computed: {
     product() {
